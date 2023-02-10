ee4c77824affdc823c2558b746b7dd7a976adcee
변성도 <dmdsns@naver.com>
2023-02-10 20:31:53 +0900

    0210_dooo

--- softeer/순위.py
+++ softeer/순위.py
@@ -1,4 +1,19 @@
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
 lst = [0] + lst
-cnt = [0] * (n+1)
+cnt = [1] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    if lst[a] > lst[b]:
+        cnt[b] = 0
+    elif lst[a] < lst[b]:
+        cnt[a] = 0
+    else:
+        cnt[a] = 0
+        cnt[b] = 0
+ans = 0
+for i in range(1, n+1):
+    if cnt[i] == 1:
+        ans += 1
+print(ans)
