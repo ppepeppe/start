fab7e36aa567c5c0373e800f7da3b67cb46c4155
변성도 <dmdsns@naver.com>
2023-03-04 00:38:15 +0900

    0303 dooo

---
+++ c++/0303/0302/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b = 0;
+
+	cin >> a >> b;
+	
+	if (a > b)
+		cout << ">";
+	else if (a < b)
+		cout << "<";
+	else
+		cout << "==";
+	
+	return 0;
+}	
+
